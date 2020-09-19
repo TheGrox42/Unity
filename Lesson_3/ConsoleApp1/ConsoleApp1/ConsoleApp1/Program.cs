ad5a94525a848c8812b4df62e9035dcef9d85042
robocodebr <robocodebr@gmail.com>
2020-09-19 16:02:12 +0300

    Lesson_3

---
+++ Lesson_3/ConsoleApp1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            List<int> list = new List<int>();
+            for(int i = 0; i <= 10; i++)
+            {
+                list.Add(i);
+            }
+            /*int i = 1;
+            while (i <= 17)
+            {
+                Console.Write(i+" ");
+                i += 4;
+            }*/
+            /*int j = 0;
+            do
+            {
+                Console.Write(j + " ");
+                j += 5;
+            } while (j <=20);*/
+            /*String str2 = "26/06/2007";
+            for (int i = 0; i < str.Length; i = i + 4)
+            {
+                Console.WriteLine(str[i]);
+            }
+            for (int i = 0; i < str2.Length; i = i + 2)
+            {
+                Console.WriteLine(str2[i]);
+            }*/
+        }
+       
+    }
+}
