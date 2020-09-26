b16611f5bf3766c779008d5fd06296c22a2ad28d
robocodebr <robocodebr@gmail.com>
2020-09-26 15:52:06 +0300

    Lesson_4

---
+++ Lesson_4/ConsoleApp4/ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           List<string> l = new List<string>() { "Programming" };
+            l.Add("Sport");
+            l.Add("Learn language");
+            l.Add("Play computer games");
+            foreach(string s in l)
+            {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine(l.Count);
+            l.Clear();
+            Console.ReadKey();
+            
+        }
+    }
+}
