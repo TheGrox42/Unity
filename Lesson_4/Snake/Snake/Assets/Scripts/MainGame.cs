700a1cd6577ae93294609e12774277c758f2a0f0
robocodebr <robocodebr@gmail.com>
2020-10-10 15:54:36 +0300

    Lesson_6

--- Lesson_4/Snake/Snake/Assets/Scripts/MainGame.cs
+++ Lesson_4/Snake/Snake/Assets/Scripts/MainGame.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class MainGame : MonoBehaviour
 {
+
+    public GameObject SnakeHead;
+    GameObject SnakeObj;
+    int gameMode = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        createSnake();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+    void createSnake()
+    {
+        SnakeObj = Instantiate(SnakeHead) as GameObject;
+        SnakeObj.name = "Snake";
+        gameMode = 1;
+    }
+
+
+    void OnGUI()
+    {
+        int posX = Screen.width / 2;
+        int posY = Screen.height / 2;
+        switch (gameMode) {
+            case 0:
+                GUI.Button(new Rect(new Vector2(posX - 100, posY), new Vector2(200, 300)), "Старт игры" );
+                break;
+            case 1: break;
+        }
     }
 }
---
+++ Lesson_6/ConsoleApp6/ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "Danylo";
+            string b = "Biliy";
+            int c = 2007;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+        }
+    }
+}
